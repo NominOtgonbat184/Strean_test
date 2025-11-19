e6077807d14fa4175f0baf91de43ca7fa7664554
NominOtgonbat184 <2025184@student.cct.ie>
2025-11-19 15:08:00 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -15,6 +15,6 @@ def load_data(nrows):
          data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
          return data
 
-data_load_state=st.text("Loading Data...")
+data_load_state=st.text("Loading data...")
 data = load_data(10000)
-data_load_state=st.text("Loading Data...Done!")
+data_load_state=st.text("Loading data...Done!")
